e3ac92a454e05b62d924e34baf8fb218646fed1a
Billie Silvaner <quiinthieu@gmail.com>
2021-10-31 15:57:33 +0700

    Version 1.0.0 ✅

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fe'),
+      dir: require('path').join(__dirname, './coverage/demo1'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { AdminModule } from './app/admin/admin.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AdminModule)
+  .catch((err) => console.error(err));
