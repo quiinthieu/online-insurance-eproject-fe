57dc74bb55f7ba9f4abf093365bf16dd0cebc217
Billie Silvaner <quiinthieu@gmail.com>
2021-11-08 21:25:02 +0700

    Version 1.0.6 ✅

--- src/app/services/insurance-type.service.ts
+++ src/app/services/insurance-type.service.ts
@@ -2,26 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { InsuranceType } from '../entities/insurance-type.entity';
+import {HttpObservablesService} from "./http-method/http-observables.service";
 
 @Injectable()
 export class InsuranceTypeService {
   private BASE_URL:string =  environment.BASE_URL + '/insurance-type/' ;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpObservablesService) {}
 
 
   details(id: number) {
     return this.httpClient
-      .get(this.BASE_URL + 'insurance-type-details/' + id)
-      .toPromise()
-      .then((res) => res as InsuranceType);
+      .getUrl(this.BASE_URL + 'insurance-type-details/' + id);
   }
 
   findAll() {
     return this.httpClient
-      .get(this.BASE_URL + 'all-insurance-types')
-      .toPromise()
-      .then((res) => res as InsuranceType[]);
+      .getUrl(this.BASE_URL + 'all-insurance-types');
   }
 
 
