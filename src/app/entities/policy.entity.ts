8f504ef3e72c570e69468f331c16e208f9c29556
Nguyen Gia Tuan <nguyengiatuan@Nguyens-MacBook-Pro.local>
2021-11-08 22:18:14 +0700

    update life insurance

---
+++ src/app/entities/policy.entity.ts
@@ -0,0 +1,10 @@
+export class Policy {
+    id: number;
+    insuranceTypeId : number;
+    insuranceTypeName: string;
+    name :string;
+    term : number;
+    amount : number;
+    interestRate : number;
+    description :string;
+}
--- src/app/user/components/life-insurance/life-insurance.component.ts
+++ src/app/user/components/life-insurance/life-insurance.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Policy } from 'src/app/entities/policy.entity';
+import { PolicyService } from 'src/app/services/policy.service';
 
 @Component({
   selector: 'app-life-insurance',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LifeInsuranceComponent implements OnInit {
 
-  constructor() { }
+  constructor( private _policyService:PolicyService) { }
+  lifePolicy : Policy[]
 
   ngOnInit() {
+    this.findAllByInsuranceId(1);
+  }
+
+  findAllByInsuranceId(id:number) {
+    this._policyService.findAllByInsuranceType(id).then (
+      res => {
+        console.log(res);
+        for(let i =0;i<res.length;i++) {
+          res[i].amount = Math.round(res[i].amount/12);
+        }
+        this.lifePolicy = res;
+      },
+      error => {
+        return null;
+      }
+    );
   }
 
 }
