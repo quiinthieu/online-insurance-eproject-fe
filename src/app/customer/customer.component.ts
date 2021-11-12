d7e8a831bfc67d51b67cd258de971bd05186c807
nle55 <nhanhuu.le@dxc.com>
2021-11-12 12:32:26 +0700

    layout customer module

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,15 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { LayoutService } from '../admin/_metronic/layout';
+import { LayoutInitService } from '../admin/_metronic/layout/core/layout-init.service';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.scss']
+})
+export class CustomerComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+
+}
--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserComponent } from './user.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { ErrorComponent } from './components/error/error.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
-import { LifeInsuranceComponent } from './components/life-insurance/life-insurance.component';
-import { HealthInsuranceComponent } from './components/health-insurance/health-insurance.component';
-import { MotorInsuranceComponent } from './components/motor-insurance/motor-insurance.component';
-import { HomeComponent } from './components/home/home.component';
-import { CustomerGuard } from '../services/guard/customer.guard';
 import { LoginGuard } from '../services/guard/login.guard';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ActivateAccountComponent } from './components/activate-account/activate-account.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ErrorComponent } from './components/error/error.component';
+import { HealthInsuranceComponent } from './components/health-insurance/health-insurance.component';
 import { HomeInsuranceComponent } from './components/home-insurance/home-insurance.component';
+import { HomeComponent } from './components/home/home.component';
+import { LifeInsuranceComponent } from './components/life-insurance/life-insurance.component';
+import { LoginComponent } from './components/login/login.component';
+import { MotorInsuranceComponent } from './components/motor-insurance/motor-insurance.component';
+import { RegisterComponent } from './components/register/register.component';
 import { RequestEmailComponent } from './components/request-email/request-email.component';
+import { UserComponent } from './user.component';
 
 const routes: Routes = [
   {
@@ -24,14 +23,6 @@ const routes: Routes = [
       {
         path: '',
         component: HomeComponent,
-      },
-      {
-        path: 'customer',
-        canActivate: [CustomerGuard],
-        loadChildren: () =>
-          import('./customer/customer.module').then(
-            (m) => m.CustomerModule
-          ),
       },
       {
         path: 'about-us',
