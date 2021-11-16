c633bb5e2567fd595421d4eaf610ec99e7883aec
giatunn123@gmail.com <giatunn123@gmail.com>
2021-11-16 21:37:31 +0700

    front-end fixed

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -12,30 +12,30 @@ import { CustomerService } from '../services/customer.service';
   styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit {
-  customer : Customer;
+  // customer : Customer;
   constructor(
-    private customerService : CustomerService,
-    private formBuilder: FormBuilder,
-    private toastr: ToastrService
+    // private customerService : CustomerService,
+    // private formBuilder: FormBuilder,
+    // private toastr: ToastrService
   ) { }
 
   ngOnInit(): void {
-    this.customer = new Customer();
-    this.loadProfile();
+    // this.customer = new Customer();
+    // this.loadProfile();
   }
-  loadProfile() {
-    const credential = JSON.parse(localStorage.getItem("credential"));
-    console.log("Credential id: "+credential.id);
-    this.customerService.detailsbycredentialid(credential.id).then(
-      res => {
-        console.log(res);
-        this.customer = res;
-        console.log(this.customer);
-        this.toastr.success("Welcome back");
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
+  // loadProfile() {
+  //   const credential = JSON.parse(localStorage.getItem("credential"));
+  //   console.log("Credential id: "+credential.id);
+  //   this.customerService.detailsbycredentialid(credential.id).then(
+  //     res => {
+  //       console.log(res);
+  //       this.customer = res;
+  //       console.log(this.customer);
+  //       this.toastr.success("Welcome back");
+  //     },
+  //     error => {
+  //       console.log(error);
+  //     }
+  //   );
+  // }
 }
